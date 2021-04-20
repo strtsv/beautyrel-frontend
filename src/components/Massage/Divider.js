44ade0f52110342dfeac6d4710606ac0c2f392ce
jsusilo <enr@bk.ru>
2021-04-20 10:06:50 +0500

    Divider massage

---
+++ src/components/Massage/Divider.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Divider extends React.Component {
+  render() {
+    return (
+      <div className="section section-dv">
+        <div className="container wide">
+          <div className="divider" />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Divider;
