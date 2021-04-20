5f1edc4931618eb97aa954586a82e058d0b449d5
jsusilo <enr@bk.ru>
2021-04-20 10:07:01 +0500

    Services massage

---
+++ src/components/Massage/Services.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+class Services extends React.Component {
+  render() {
+    return (
+      <div className="section section-lg bg-secondary">
+        <div className="container wide">
+          <div className="text-center">
+            <h1>Services</h1>
+            <div className="subtitle-2">
+              Lorem ipsum dolor sit amet, consectetur
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Services;
