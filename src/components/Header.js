634a5f3edcb703e6d1a04edec5de518d6cace56d
jsusilo <enr@bk.ru>
2021-04-20 10:06:07 +0500

    common header

---
+++ src/components/Header.js
@@ -0,0 +1,178 @@
+import React from "react";
+import $ from "jquery";
+import { WOW } from "../assets/js/wow.js";
+import "../assets/js/tmstickup.js";
+import "../assets/js/parallax.js";
+
+import i1 from "../assets/images/logo-default-428x112.png";
+import i2 from "../assets/images/logo-inverse-430x112.png";
+
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    $(document).ready(function() {
+      new WOW().init();
+      $(".rd-navbar").TMStickUp({});
+    });
+  }
+  render() {
+    return (
+      <header className="section page-header">
+        {}
+        <div className="rd-navbar-wrap">
+          <nav
+            className="rd-navbar rd-navbar-classic rd-navbar-original rd-navbar-static"
+            data-layout="rd-navbar-fixed"
+            data-sm-layout="rd-navbar-fixed"
+            data-md-layout="rd-navbar-fixed"
+            data-md-device-layout="rd-navbar-fixed"
+            data-lg-layout="rd-navbar-static"
+            data-lg-device-layout="rd-navbar-static"
+            data-xl-layout="rd-navbar-static"
+            data-xl-device-layout="rd-navbar-static"
+            data-lg-stick-up-offset="46px"
+            data-xl-stick-up-offset="46px"
+            data-xxl-stick-up-offset="46px"
+            data-lg-stick-up="true"
+            data-xl-stick-up="true"
+            data-xxl-stick-up="true"
+          >
+            <div className="rd-navbar-main-outer">
+              <div className="rd-navbar-main">
+                {}
+                <div className="rd-navbar-panel">
+                  {}
+                  <button
+                    className="rd-navbar-toggle"
+                    data-rd-navbar-toggle=".rd-navbar-nav-wrap"
+                  >
+                    <span />
+                  </button>
+                  {}
+                  <div className="rd-navbar-brand">
+                    {}
+                    <a className="brand" href="index.html">
+                      <img
+                        className="brand-logo-dark"
+                        src={i1}
+                        alt
+                        width={214}
+                        height={56}
+                      />
+                      <img
+                        className="brand-logo-light"
+                        src={i2}
+                        alt
+                        width={215}
+                        height={56}
+                      />
+                    </a>
+                  </div>
+                </div>
+                <div className="rd-navbar-main-element">
+                  <div className="rd-navbar-nav-wrap">
+                    <ul className="rd-navbar-nav">
+                      <li className="rd-nav-item active">
+                        <a className="rd-nav-link" href="index.html">
+                          Home
+                        </a>
+                      </li>
+                      <li className="rd-nav-item">
+                        <a className="rd-nav-link" href="gift-vouchers.html">
+                          Gift Vouchers
+                        </a>
+                      </li>
+                      <li className="rd-nav-item">
+                        <a className="rd-nav-link" href="about.html">
+                          About
+                        </a>
+                      </li>
+                      <li className="rd-nav-item">
+                        <a className="rd-nav-link" href="massage.html">
+                          Massage
+                        </a>
+                        <ul className="rd-menu rd-navbar-dropdown">
+                          <li className="rd-dropdown-item">
+                            <a
+                              className="rd-dropdown-link"
+                              href="single-massage.html"
+                            >
+                              Single Massage
+                            </a>
+                          </li>
+                        </ul>
+                      </li>
+                      <li className="rd-nav-item">
+                        <a className="rd-nav-link" href="contacts.html">
+                          Contacts
+                        </a>
+                      </li>
+                      <li className="rd-nav-item">
+                        <a className="rd-nav-link" href="#">
+                          Pages
+                        </a>
+                        <ul className="rd-menu rd-navbar-dropdown">
+                          <li className="rd-dropdown-item">
+                            <a
+                              className="rd-dropdown-link"
+                              href="typography.html"
+                            >
+                              Typography
+                            </a>
+                          </li>
+                          <li className="rd-dropdown-item">
+                            <a className="rd-dropdown-link" href="buttons.html">
+                              Buttons
+                            </a>
+                          </li>
+                          <li className="rd-dropdown-item">
+                            <a className="rd-dropdown-link" href="forms.html">
+                              Forms
+                            </a>
+                          </li>
+                          <li className="rd-dropdown-item">
+                            <a
+                              className="rd-dropdown-link"
+                              href="tabs-and-accordions.html"
+                            >
+                              Tabs and accordions
+                            </a>
+                          </li>
+                          <li className="rd-dropdown-item">
+                            <a
+                              className="rd-dropdown-link"
+                              href="privacy-policy.html"
+                            >
+                              Privacy policy
+                            </a>
+                          </li>
+                          <li className="rd-dropdown-item">
+                            <a
+                              className="rd-dropdown-link"
+                              href="under-construction.html"
+                            >
+                              Under Construction
+                            </a>
+                          </li>
+                        </ul>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <div className="rd-nav-item">
+                  <div className="btn-wrap">
+                    <a className="button button-secondary button-sm" href="#">
+                      Get A Massage
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </nav>
+        </div>
+      </header>
+    );
+  }
+}
+
+export default Header;
