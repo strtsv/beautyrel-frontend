52c721782c045c07f8bf81cf4885693117cd6270
jsusilo <enr@bk.ru>
2021-04-22 10:05:52 +0500

    contacts content comp

---
+++ src/components/Contacts/Content.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+class Content extends React.Component {
+  render() {
+    return (
+      <section className="section-lg bg-secondary">
+        <div className="container wide">
+          <div className="text-center">
+            <h1>Contacts</h1>
+            <div className="subtitle-2">
+              Lorem ipsum dolor sit amet, consectetur
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default Content;
