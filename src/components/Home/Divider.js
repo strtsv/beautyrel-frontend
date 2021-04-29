fb6970940f080592771ea349e27d87546b7557d4
jsusilo <enr@bk.ru>
2021-04-29 21:43:41 +0500

    home comp div

---
+++ src/components/Home/Divider.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Divider extends React.Component {
+  render() {
+    return (
+      <div className="section">
+        <div className="container wide">
+          <div className="divider" />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Divider;
