e79da0ad731049153d90e32cbbc3a6312cb58ec5
jsusilo <enr@bk.ru>
2021-04-29 21:43:49 +0500

    home comp faq

---
+++ src/components/Home/FAQ.js
@@ -0,0 +1,217 @@
+import React from "react";
+
+class FAQ extends React.Component {
+  render() {
+    return (
+      <div className="section section section-xl">
+        <div className="container wide">
+          <div className="row row-md-80 row-sm-50">
+            <div className="col-sm-12 col-lg-4">
+              <div className="subtitle">
+                Frequently Asked <br className="br-none" />
+                Questions
+              </div>
+            </div>
+            <div className="col-sm-12 col-lg-8">
+              <div
+                className="card-group-custom"
+                id="accordion1"
+                role="tablist"
+                aria-multiselectable="false"
+              >
+                {}
+                <article className="card card-custom card-group-custom card-corporate">
+                  <div className="card-heading" role="tab">
+                    <div className="card-title">
+                      <a
+                        className="collapsed"
+                        id="accordion1-card-head-scxqajyi"
+                        data-toggle="collapse"
+                        data-parent="#accordion1"
+                        href="#accordion1-card-body-fiucadpr"
+                        aria-controls="accordion1-card-body-fiucadpr"
+                        aria-expanded="false"
+                        role="button"
+                      >
+                        Do you provide any scripts with your templates?
+                      </a>
+                    </div>
+                  </div>
+                  <div
+                    className="card-collapse collapse"
+                    id="accordion1-card-body-fiucadpr"
+                    aria-labelledby="accordion1-card-head-scxqajyi"
+                    data-parent="#accordion1"
+                    role="tabpanel"
+                  >
+                    <div className="card-body">
+                      <p>
+                        Our templates do not include any additional scripts.
+                        Newsletter subscriptions, search fields, forums, image
+                        galleries (in HTML versions of Flash products) are
+                        inactive. Basic scripts can be easily added at
+                        www.zemez.io If you are not sure that the element you’re
+                        interested in is active please contact our Support Chat
+                        for clarification.
+                      </p>
+                    </div>
+                  </div>
+                </article>
+                {}
+                <article className="card card-custom card-group-custom card-corporate">
+                  <div className="card-heading" role="tab">
+                    <div className="card-title">
+                      <a
+                        className="collapsed"
+                        id="accordion1-card-head-lhyfsgto"
+                        data-toggle="collapse"
+                        data-parent="#accordion1"
+                        href="#accordion1-card-body-cuweomyl"
+                        aria-controls="accordion1-card-body-cuweomyl"
+                        aria-expanded="false"
+                        role="button"
+                      >
+                        What are the advantages of purchasing a website
+                        template?
+                      </a>
+                    </div>
+                  </div>
+                  <div
+                    className="card-collapse collapse"
+                    id="accordion1-card-body-cuweomyl"
+                    aria-labelledby="accordion1-card-head-lhyfsgto"
+                    data-parent="#accordion1"
+                    role="tabpanel"
+                  >
+                    <div className="card-body">
+                      <p>
+                        The major advantage is price: You get a high quality
+                        design for just $20-$70. You don’t have to hire a web
+                        designer or web design studio. Second advantage is time
+                        frame: It usually takes 5-15 days for a good designer to
+                        produce a web page of such quality.
+                      </p>
+                    </div>
+                  </div>
+                </article>
+                {}
+                <article className="card card-custom card-group-custom card-corporate">
+                  <div className="card-heading" role="tab">
+                    <div className="card-title">
+                      <a
+                        className="collapsed"
+                        id="accordion1-card-head-pektlydn"
+                        data-toggle="collapse"
+                        data-parent="#accordion1"
+                        href="#accordion1-card-body-nmbkplic"
+                        aria-controls="accordion1-card-body-nmbkplic"
+                        aria-expanded="false"
+                        role="button"
+                      >
+                        What do I receive when I order a template from Zemez?
+                      </a>
+                    </div>
+                  </div>
+                  <div
+                    className="card-collapse collapse"
+                    id="accordion1-card-body-nmbkplic"
+                    aria-labelledby="accordion1-card-head-pektlydn"
+                    data-parent="#accordion1"
+                    role="tabpanel"
+                  >
+                    <div className="card-body">
+                      <p>
+                        After you complete the payment via our secure form you
+                        will receive the instructions for downloading the
+                        product. The source files in the download package can
+                        vary based on the type of the product you have
+                        purchased.
+                      </p>
+                    </div>
+                  </div>
+                </article>
+                {}
+                <article className="card card-custom card-group-custom card-corporate">
+                  <div className="card-heading" role="tab">
+                    <div className="card-title">
+                      <a
+                        className="collapsed"
+                        id="accordion1-card-head-ulgwsbdb"
+                        data-toggle="collapse"
+                        data-parent="#accordion1"
+                        href="#accordion1-card-body-uktoaeet"
+                        aria-controls="accordion1-card-body-uktoaeet"
+                        aria-expanded="false"
+                        role="button"
+                      >
+                        In what formats are your templates available?
+                      </a>
+                    </div>
+                  </div>
+                  <div
+                    className="card-collapse collapse"
+                    id="accordion1-card-body-uktoaeet"
+                    aria-labelledby="accordion1-card-head-ulgwsbdb"
+                    data-parent="#accordion1"
+                    role="tabpanel"
+                  >
+                    <div className="card-body">
+                      <p>
+                        Website templates are available in Photoshop and HTML
+                        formats. Fonts are included with the Photoshop file. In
+                        most templates, HTML is compatible with Adobe®
+                        Dreamweaver® and Microsoft® FrontPage®.
+                      </p>
+                    </div>
+                  </div>
+                </article>
+                {}
+                <article className="card card-custom card-group-custom card-corporate">
+                  <div className="card-heading" role="tab">
+                    <div className="card-title">
+                      <a
+                        className="collapsed"
+                        id="accordion1-card-head-ahsbcrxn"
+                        data-toggle="collapse"
+                        data-parent="#accordion1"
+                        href="#accordion1-card-body-mxjgdwwh"
+                        aria-controls="accordion1-card-body-mxjgdwwh"
+                        aria-expanded="false"
+                        role="button"
+                      >
+                        What am I allowed to do with the templates?
+                      </a>
+                    </div>
+                  </div>
+                  <div
+                    className="card-collapse collapse"
+                    id="accordion1-card-body-mxjgdwwh"
+                    aria-labelledby="accordion1-card-head-ahsbcrxn"
+                    data-parent="#accordion1"
+                    role="tabpanel"
+                  >
+                    <div className="card-body">
+                      <p>
+                        You may build a website using the template in any way
+                        you like. You may not resell or redistribute templates
+                        (like we do); claim intellectual or exclusive ownership
+                        to any of our products, modified or unmodified. All
+                        products are property of content providing companies and
+                        individuals. You are also not allowed to make more than
+                        one project using the same template (you have to
+                        purchase the same template once more in order to make
+                        another project with the same design).
+                      </p>
+                    </div>
+                  </div>
+                </article>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default FAQ;
